92974c833fa6a16e1e9c0d6c2adb5c986bb30497
bereluna3 <berebluna@gmail.com>
2024-12-01 18:22:08 -0600

    Se añaden tipos de datos

---
+++ Tipo-de-datos.js/Tipo-de-datos.js
@@ -0,0 +1,15 @@
+console.log(typeof 42); // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object (curiosidad del lenguaje)
+console.log(typeof undefined); // undefined
+console.log(typeof false); // boolean
+
+// Ejemplos adicionales
+console.log(typeof []); // object (los arrays son objetos en JS)
+console.log(typeof {}); // object
+console.log(typeof Symbol('simbolo')); // symbol
+console.log(typeof function(){}); // function
